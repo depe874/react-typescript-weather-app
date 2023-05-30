93a404ca4aef44baead2fb93502092d8f6b7f47f
S.Morimoto <72482515+depe874@users.noreply.github.com>
2023-05-30 21:59:26 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from "react";
+import Title from './components/Title';
+import Form from './components/Form';
+import Results from './components/Results';
 import './App.css';
 
+// 型定義
+type ResultsStateType = {
+  country:string;
+  cityName:string;
+  temperature:string;
+  conditionText:string;
+  icon:string;
+}
+
 function App() {
+  // cityにはユーザが入力した都市名(state)
+  // setCityはcity内のデータを操作する
+  // useStateの()内は初期データ
+  // cityはstring型
+  const [city, setCity] = useState<string>("");
+
+  // 複数のデータを保管するため，入れ子構造
+  const [results, setResults] = useState<ResultsStateType>({
+    country:"",
+    cityName:"",
+    temperature:"",
+    conditionText:"",
+    icon:""
+  });
+  
+  // 型が分からないときはanyを指定
+  // このeはTypeScriptの型ではなくReactが定義している型
+  const getWeather = (e:React.FormEvent<HTMLFormElement>) => {
+    // buttonを押してもリロードが発生しない
+    e.preventDefault();
+    // fetchはAPIをコールする
+    // バッククオートと$を使ってcityを代入する
+    fetch(`https://api.weatherapi.com/v1/current.json?key=f7a17ad90d344eae8ff50010233005&q=${city}&aqi=no`)
+    // resはAPIから送られてくる気象データ
+    // console.log()はデータをコンソールに表示させる
+    //.then(res => console.log(res))
+
+    // resをjson形式のデータに変換
+    .then(res => res.json())
+    //.then(data => console.log(data))
+    .then(data => {
+      setResults({
+        country:data.location.country,
+        cityName:data.location.name,
+        temperature:data.current.temp_c,
+        conditionText:data.current.condition.text,
+        icon:data.current.condition.icon
+      })
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+      <Title/>
+      {/*タグの中に渡すものとその名前を書くと，
+         そのコンポーネントにpropsを渡せる */}
+      <Form setCity={setCity} getWeather={getWeather}/>
+      <Results results={results}/>
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,46 @@
+// 型定義
+type FormPropsType={
+    setCity: React.Dispatch<React.SetStateAction<string>>
+    getWeather: (e:React.FormEvent<HTMLFormElement>) => void
+}
+
+// propsとして渡すものにも型を設定する必要がある
+const Form = (props:FormPropsType) =>{
+    // // cityにはユーザが入力した都市名(state)
+    // // setCityはcity内のデータを操作する
+    // // useStateの()内は初期データ
+    // // cityはstring型
+    // const [city, setCity] = useState<string>("");
+    
+    // // 型が分からないときはanyを指定
+    // const getWeather = (e:any) => {
+    //     // buttonを押してもリロードが発生しない
+    //     e.preventDefault();
+    //         // fetchはAPIをコールする
+    //         fetch("https://api.weatherapi.com/v1/current.json?key=f7a17ad90d344eae8ff50010233005&q=London&aqi=no")
+    //         // resはAPIから送られてくる気象データ
+    //         // console.log()はデータをコンソールに表示させる
+    //         //.then(res => console.log(res))
+
+    //         // resをjson形式のデータに変換
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // }
+    // ↑この部分をApp.tsxに移す
+
+    return(
+        <form onSubmit={props.getWeather}>
+            {/* onChangeハンドラーは入力されたデータをsetCityに渡す
+                eはevent parameter/event object
+                入力された情報はeの中のtargetの中のvalueに格納されている
+                setCityでe.target.valueのデータをcityに保管する */}
+            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
+            {/*onClickハンドラーを使ってbuttonを押すときにgetWeatherを呼び出す
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
+            ただしブラウザによっては正しく動作しない場合があるのでonSubmitに置き換える*/}
+            <button type="submit">Get Weather</button>
+        </form>
+    );
+};
+
+export default Form;
